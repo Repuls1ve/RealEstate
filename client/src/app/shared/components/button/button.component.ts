149a52fb8d9bc1d9a4e1dfc18c9fc5cb0945f0f9
Repuls1ve <repulsive@list.ru>
2022-03-29 18:56:07 +0300

    shared button colors

--- client/src/app/shared/components/button/button.component.ts
+++ client/src/app/shared/components/button/button.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostBinding, Input } from '@angular/core'
 
 export type ButtonTheme = 'contained' | 'outlined'
+export type ButtonColor = 'primary' | 'secondary'
 
 @Component({
   selector: '[shared-button]',
@@ -11,8 +12,11 @@ export class ButtonComponent {
   @Input()
   public theme: ButtonTheme = 'contained'
 
+  @Input()
+  public color: ButtonColor = 'primary'
+
   @HostBinding('class')
   public get classes() {
-    return this.theme
+    return `${this.theme} ${this.color}`
   }
 }
