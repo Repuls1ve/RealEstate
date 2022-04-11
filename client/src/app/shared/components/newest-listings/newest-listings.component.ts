e90f406b1e7a92ef22b0e3fb872316f6e54889f4
Repuls1ve <repulsive@list.ru>
2022-04-11 12:16:11 +0300

    newest listings loader & store

--- client/src/app/shared/components/newest-listings/newest-listings.component.ts
+++ client/src/app/shared/components/newest-listings/newest-listings.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { NewestListingsStore } from './newest-listings.store'
 
 @Component({
   selector: 'newest-listings',
   templateUrl: './newest-listings.component.html',
-  styleUrls: ['./newest-listings.component.scss']
+  styleUrls: ['./newest-listings.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [NewestListingsStore]
 })
-export class NewestListingsComponent {}
+export class NewestListingsComponent {
+  public readonly vm$ = this.newestListingsStore.vm$
+
+  @Input()
+  public set quantity(value: number) {
+    this.newestListingsStore.fetchListings(value)
+  }
+
+  constructor(private readonly newestListingsStore: NewestListingsStore) {}
+}
