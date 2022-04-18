e99cee6c4e3df443345de43b1d6b5424f24051c2
Repuls1ve <repulsive@list.ru>
2022-04-18 20:13:18 +0300

    times pipe spec

--- client/src/app/shared/pipes/times/times.pipe.spec.ts
+++ client/src/app/shared/pipes/times/times.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { TimesPipe } from './times.pipe'
 
 describe('TimesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimesPipe()
-    expect(pipe).toBeTruthy()
+  const pipe = new TimesPipe()
+
+  it('should transform non-negative number into array', () => {
+    expect(pipe.transform(3)).toBeInstanceOf(Array)
+  })
+
+  it('should transform positive number into array with the appropriate length', () => {
+    expect(pipe.transform(1).length).toBe(1)
+  })
+
+  it('should transform zero into empty array', () => {
+    expect(pipe.transform(0)).toEqual([])
+  })
+
+  it('should throw an error when trying to use negative number', () => {
+    expect(() => pipe.transform(-1)).toThrowError()
   })
 })
