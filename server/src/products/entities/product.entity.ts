ef9c43e0cc026979f8ea963f9186d825fc9fbc1e
Repuls1ve <repulsive@list.ru>
2022-04-26 18:24:54 +0300

    product entity

---
+++ server/src/products/entities/product.entity.ts
@@ -0,0 +1,38 @@
+import { AgencyEntity } from '../../agencies/entities/agency.entity'
+
+export interface ProductEntity {
+  readonly title: string
+  readonly location: ProductLocation
+  readonly description: string
+  readonly overview: string[]
+  readonly details: ProductDetails
+  readonly photos: Photo[]
+  readonly agency: AgencyEntity
+}
+
+export interface ProductLocation {
+  readonly position: string
+  readonly address: Address
+}
+
+export interface ProductDetails {
+  readonly uid: string
+  readonly price: number
+  readonly size: string
+  readonly category: string
+  readonly status: string
+  readonly year: number
+}
+
+export interface Address {
+  readonly city: string
+  readonly state: string
+  readonly zip: string
+  readonly area: string
+  readonly country: string
+}
+
+export interface Photo {
+  readonly source: string
+  readonly alt: string
+}
