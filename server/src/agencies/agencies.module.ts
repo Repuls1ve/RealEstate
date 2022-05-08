5582f3b8ffcc2fc77c8ed761d64488c70d3c358b
Repuls1ve <repulsive@list.ru>
2022-05-08 20:43:33 +0300

    feat: #23 agency entity & model

--- server/src/agencies/agencies.module.ts
+++ server/src/agencies/agencies.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AgenciesService } from './services/agencies.service'
 import { AgenciesController } from './controllers/agencies.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AgencyModel } from './schemas/agency.schema'
 
 @Module({
+  imports: [MongooseModule.forFeature([AgencyModel])],
   providers: [AgenciesService],
   controllers: [AgenciesController],
   exports: [AgenciesService]
