40fe011442c50f584ada05bf52b70354c70814a3
Repuls1ve <repulsive@list.ru>
2022-05-09 18:17:41 +0300

    wip: #25 stepper ui component

---
+++ client/src/app/shared/ui/stepper/stepper.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { ReactiveComponentModule } from '@ngrx/component'
+import { StepModule } from './step/step.module'
+import { StepperComponent } from './stepper.component'
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StepModule,
+    ReactiveComponentModule
+  ],
+  declarations: [StepperComponent],
+  exports: [StepperComponent]
+})
+export class StepperModule {}
---
+++ client/src/app/shared/ui/stepper/stepper.store.ts
@@ -0,0 +1,45 @@
+import { EventEmitter, Injectable } from '@angular/core'
+import { ComponentStore } from '@ngrx/component-store'
+import { map, Observable, tap } from 'rxjs'
+
+export interface Step {
+  readonly id: number
+  readonly label: string
+  readonly isActive: boolean
+}
+
+export interface StepperState {
+  readonly steps: Step[]
+}
+
+@Injectable()
+export class StepperStore extends ComponentStore<StepperState> {
+  constructor() {
+    super({
+      steps: []
+    })
+  }
+
+  public readonly stepsChange$ = new EventEmitter<Step[]>()
+
+  public readonly setSteps = this.updater((state, value: Step[]) => ({
+    ...state,
+    steps: value
+  }))
+
+  public readonly vm$ = this.select(store => store)
+
+  public readonly changeSteps = this.effect((steps$: Observable<Step[]>) => steps$.pipe(
+    tap(steps => this.setSteps(steps)),
+    tap(steps => this.stepsChange$.emit(steps))
+  ))
+
+  public readonly onStepChange = this.effect((step$: Observable<Step>) => step$.pipe(
+    map(step => this.get().steps.map(target => target.id == step.id ?
+      { ...step, isActive: true } :
+      { ...target, isActive: false })
+    ),
+    tap(steps => this.setSteps(steps)),
+    tap(steps => this.stepsChange$.emit(steps))
+  ))
+}
