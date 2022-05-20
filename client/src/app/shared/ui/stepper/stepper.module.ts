25cba677c940d921e311355a9e9d4704dae00fc6
Repuls1ve <repulsive@list.ru>
2022-05-20 17:48:15 +0300

    wip: #50 removed redundant module for step subcomponent

--- client/src/app/shared/ui/stepper/stepper.module.ts
+++ client/src/app/shared/ui/stepper/stepper.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { ReactiveComponentModule } from '@ngrx/component'
-import { StepModule } from './step/step.module'
+import { StepComponent } from './step/step.component'
 import { StepperComponent } from './stepper.component'
 
 @NgModule({
-  imports: [CommonModule, StepModule, ReactiveComponentModule],
-  declarations: [StepperComponent],
+  imports: [CommonModule, ReactiveComponentModule],
+  declarations: [StepperComponent, StepComponent],
   exports: [StepperComponent]
 })
 export class StepperModule {}
